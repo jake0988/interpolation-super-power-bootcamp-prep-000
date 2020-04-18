46538196fec6e688df19da02f4b3c8e363afe687
Jake G <jake098877@gmail.com>
2020-04-18 02:22:02 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,10 +2,10 @@ def display_rainbow(num)
   # colors = ['R: red', 'O: orange', 'Y: yellow', 'G: green', 'B: blue', 'I: indigo', 'V: violet']
   colors = ['R: ', 'O: ', 'Y: ', 'G: ', 'B: ', 'I: ', 'V: ']
   print "R: #{num[0]}, "
-  puts "#{colors[1]} #{num[1]}"
-  puts "#{colors[2]} #{num[2]}"
-  puts "#{colors[3]} #{num[3]}"
-  puts "#{colors[4]} #{num[4]}"
-  puts "#{colors[5]} #{num[5]}"
-  puts "#{colors[6]} #{num[6]}"
+  print "#{colors[1]} #{num[1]}"
+  print "#{colors[2]} #{num[2]}"
+  print "#{colors[3]} #{num[3]}"
+  print "#{colors[4]} #{num[4]}"
+  print "#{colors[5]} #{num[5]}"
+  print "#{colors[6]} #{num[6]}"
 end
